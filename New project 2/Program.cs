4b7e0fe7e3fe7378f0fc32a466533073dca048ef
USER <USER@DESKTOP-SNMHLDN>
2024-01-12 18:01:59 +0100

    Add project files.

---
+++ New project 2/Program.cs
@@ -0,0 +1,16 @@
+﻿public  class Program
+{
+    private static void Main(string[] args)
+    {
+       
+        Console.WriteLine("Hello, Love ");
+        Console.WriteLine("Welcome ");
+        WelcomeHome();
+    }
+    private static void WelcomeHome()
+    {
+
+        Console.WriteLine("Welcome to my world,free drinks on the house!");
+            
+    }
+}
